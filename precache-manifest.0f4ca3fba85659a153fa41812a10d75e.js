6c6cbe77d377f5d8169277dd293f64f893293db7
hafizsheetab <muhtaseen@iut-dhaka.edu>
2020-10-15 04:08:46 +0600

    Updates

---
+++ precache-manifest.0f4ca3fba85659a153fa41812a10d75e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84fce2ef8ba365cf713e417f80c53f05",
+    "url": "/MeetMyTeam/index.html"
+  },
+  {
+    "revision": "5d6593b665b7112e46c9",
+    "url": "/MeetMyTeam/static/css/main.254e1f1e.chunk.css"
+  },
+  {
+    "revision": "a5d203ff7a8ac413be86",
+    "url": "/MeetMyTeam/static/js/2.be7ebba4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MeetMyTeam/static/js/2.be7ebba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d6593b665b7112e46c9",
+    "url": "/MeetMyTeam/static/js/main.d2df45eb.chunk.js"
+  },
+  {
+    "revision": "d9504ae40fbdb73f85ac",
+    "url": "/MeetMyTeam/static/js/runtime-main.4a38563c.js"
+  },
+  {
+    "revision": "685f32027792b38c65910e2a623d8606",
+    "url": "/MeetMyTeam/static/media/background.685f3202.jpg"
+  },
+  {
+    "revision": "fd0d5546fdbdc85c76c4372a0d51f1bc",
+    "url": "/MeetMyTeam/static/media/linkedin.fd0d5546.png"
+  },
+  {
+    "revision": "48e0185f3c3399e502d8960cd114871b",
+    "url": "/MeetMyTeam/static/media/pranto.48e0185f.jpg"
+  },
+  {
+    "revision": "47425ad419535fb38209b68e389d711f",
+    "url": "/MeetMyTeam/static/media/prima_1.47425ad4.jpg"
+  },
+  {
+    "revision": "59144a6f1d61be4f0c815f1de7342bdb",
+    "url": "/MeetMyTeam/static/media/shafi.59144a6f.jpg"
+  },
+  {
+    "revision": "cd8bfbe2f79185fcfbc91d1540dac175",
+    "url": "/MeetMyTeam/static/media/sheetab.cd8bfbe2.jpg"
+  }
+]);
